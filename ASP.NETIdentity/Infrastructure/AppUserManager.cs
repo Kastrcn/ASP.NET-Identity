d958d6aab2f17413a3f135524e148776c6d3d217
Kastrcn <Kastrcn@WEIYOU-PRO>
2018-01-10 16:47:40 +0800

    Identity 增删改查

--- ASP.NETIdentity/Infrastructure/AppUserManager.cs
+++ ASP.NETIdentity/Infrastructure/AppUserManager.cs
@@ -16,12 +16,26 @@ namespace ASP.NETIdentity.Infrastructure
             : base(store)
         {
         }
+
         public static AppUserManager Create(
             IdentityFactoryOptions<AppUserManager> options,
             IOwinContext context)
         {
             AppIdentityDbContext db = context.Get<AppIdentityDbContext>();
             AppUserManager manager = new AppUserManager(new UserStore<AppUser>(db));
+            manager.PasswordValidator = new CustomPasswordValidator
+            {
+                RequiredLength = 6,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
+                RequireLowercase = true,
+                RequireUppercase = true
+            };
+            manager.UserValidator = new CustomUserValidator(manager)
+            {
+                AllowOnlyAlphanumericUserNames = true,
+                RequireUniqueEmail = true
+            };
             return manager;
         }
     }
---
+++ ASP.NETIdentity/Infrastructure/CustomPasswordValidator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.Identity;
+
+namespace ASP.NETIdentity.Infrastructure
+{
+    public class CustomPasswordValidator : PasswordValidator
+    {
+        public override async Task<IdentityResult> ValidateAsync(string pass)
+        {
+            IdentityResult result = await base.ValidateAsync(pass);
+            if (pass.Contains("12345"))
+            {
+                var errors = result.Errors.ToList();
+                errors.Add("密码不能为连续的数字");
+                result = new IdentityResult(errors);
+            }
+            return result;
+        }
+    }
+}
